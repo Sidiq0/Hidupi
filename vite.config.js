3e3a68fd9ff940c6bc13023fbd0a807218b6ca57
Sidiq0 <sidiqamarulloh008@gmail.com>
2024-07-30 21:59:59 +0700

    add home page from theme

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/style.css',
+                    'resources/js/app.js', 'resources/js/main.js', 'resources/js/validate.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources'
+        }
+    }
 });
